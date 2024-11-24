1d4b073f3e5ae359b374633a950eb7d93140eb29
robert koopedi <koopedi05@gmail.com>
2024-11-24 20:22:23 +0200

    Updated display menu items function

--- index.js
+++ index.js
@@ -8,31 +8,50 @@ const menu = {
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
+    const menuContainer = document.getElementById("menu")
 
     // Loop through each category and its items in the menu object
+    if (!menuContainer) {
+        return;  // Exit if menu container is not found
+    }
+
+    for (const category in menu) { 
 
         // Create an element to represent the category
+        const categoryElement = document.createElement('h2');
+       
 
         // Set the text content of the category element to the category name
-
+        categoryElement.textContent = category;  //Set category name
         // Append the category element to the menu container
+        menuContainer.appendChild(categoryElement);
 
         // Create an element to represent a list of items
+        const itemList = document.createElement('ul');
 
         // Append a list of items element to the menu container
+        menuContainer.appendChild(listItem);
+    
+
 
         // Loop through the items in the category and create list items
+        menu[category].forEach(item => {
 
             // Create a list item element
+            const listItem = document.createElement('li');
+           
 
             // Set the text content of the list item element to the item name
+            listItem.textContent = item;  // Set text content to the item name
 
             // Attach a click event listener to the list item to add it to the order
+            listItem.addEventListener('click', () => addToOrder(item));
+
 
             // Append the list item to the list of items
-
-            
+            itemList.appendChild(listItem);
 }
+);
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
@@ -56,3 +75,5 @@ function initMenuSystem(menu) {
 
 // Start the menu system by calling the init function
 initMenuSystem(menu);
+// console.log(menu)
+    }}
